edff42f888d372c512f33a474e023ec23f1e83fa
SlemanKhadoor <s.khadoor@zendy.io>
2025-07-01 07:11:43 +0400

    strings functions

---
+++ strings_function.py
@@ -0,0 +1,18 @@
+text = "Welcome to Python"
+print(len(text))
+
+#---------------
+
+print(text[3])
+print(text[11:])
+
+# without the last character
+print(text[11:-1])
+
+#---------------
+text.upper()
+print(text)
+print(text.upper())
+print(text.lower())
+#---------------
+print(text.replace("Python","not python"))
