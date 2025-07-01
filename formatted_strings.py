89dbf3940a6a59278df7d16522ab0da4056d7de1
SlemanKhadoor <s.khadoor@zendy.io>
2025-07-01 06:55:09 +0400

    formatted strings

---
+++ formatted_strings.py
@@ -0,0 +1,7 @@
+first_name = "John"
+last_name = "Doo"
+message ="my first name is"+ first_name +" and my last name is "+ last_name
+print(message)
+# In the previous example most of the times we might forget to add spaces or we might add many so we use the following
+message = f'my fist nmes is {first_name} and my last name is {last_name}'
+print(message)
