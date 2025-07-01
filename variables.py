d0cc6c7a612a5e1b087711b96dda15edeff9377e
SlemanKhadoor <s.khadoor@zendy.io>
2025-07-01 05:23:07 +0400

    variables

---
+++ variables.py
@@ -0,0 +1,16 @@
+name = "john"
+print(name)
+#---------------------------------
+print("Name:" + name)
+#---------------------------------
+num = 3
+print (num)
+#---------------------------------
+fraction_number = 7.5
+print(fraction_number)
+#---------------------------------
+is_bool = False
+print(is_bool)
+is_bool = True
+print(is_bool)
+#---------------------------------
