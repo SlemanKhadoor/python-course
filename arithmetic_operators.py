db5f7bdd4c88258d870cdd3671837b00bff23f70
SlemanKhadoor <s.khadoor@zendy.io>
2025-07-03 12:49:52 +0400

    arithmatic operations

---
+++ arithmetic_operators.py
@@ -0,0 +1,17 @@
+num1 = 10
+num2 = 3
+
+print(num1 - num2)
+print(num1 + num2)
+print(num1 * num2)
+print(num1 / num2)
+print(num2 / num1)
+# int division result
+print(num1 // num2)
+#Modulus
+print(num1 % num2)
+#power
+print(num1 ** num2)
+
+num1 += 10
+print (num1)
