cc251af68c9426b4349e674e85d2c2faf3ca9e92
SlemanKhadoor <s.khadoor@zendy.io>
2025-07-03 15:13:34 +0400

    math functions

---
+++ math_functions.py
@@ -0,0 +1,11 @@
+import math
+print(math.ceil(2.4))
+print(math.floor(2.9))
+# you can test all the functions by yourself
+print("############")
+# basic functions exist without importing any library
+num = 2.6
+print(num)
+print(round(num))
+print (abs(num))
+print (abs(-1))
