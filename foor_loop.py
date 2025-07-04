63f81265d8e64974e858a616a203c817603c39a9
SlemanKhadoor <s.khadoor@zendy.io>
2025-07-04 05:46:25 +0400

    foor loop

---
+++ foor_loop.py
@@ -0,0 +1,20 @@
+word = "welcome"
+for i in word:
+    print (i)
+
+print("------------------")
+for i in [1,2,3,4]:
+    print (i)
+print("------------------")
+# to print from 0 to 19
+for i in range(20):
+    print (i)
+print("------------------")
+# to print from 2 to 19
+for i in range(2,20):
+    print (i)
+print("------------------")
+# to print from 21 to 19 with step of 3
+for i in range(1,20,3):
+    print (i)
+print("------------------")
