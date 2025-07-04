de222e6ad979bbbfa9a4d708c37203253b785a5f
SlemanKhadoor <s.khadoor@zendy.io>
2025-07-04 05:22:12 +0400

    logical operations

---
+++ logical_operations.py
@@ -0,0 +1,11 @@
+is_man = True
+is_father = False
+if is_man and is_father:
+    print("man and a father")
+elif is_man & (not is_father):
+    print("man but not a father")
+else:
+    print("not a man")
+
+# we can use and or & to represent and operation
+# we can use or or | ro represent or operation
