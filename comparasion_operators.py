957921a159f621a4e72c4ddfb60f8e49de5e5fd4
SlemanKhadoor <s.khadoor@zendy.io>
2025-07-04 05:33:43 +0400

    comparasion operators

---
+++ comparasion_operators.py
@@ -0,0 +1,8 @@
+grade = 40
+
+if grade == 100:
+    print( "Great you got full mark" )
+elif grade >= 60:
+    print(" good job you passed")
+elif grade < 60:
+    print(" failed you can try next time")
