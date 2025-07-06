e1b3db372bd7862adb821dc715b667f766b281cf
SlemanKhadoor <s.khadoor@zendy.io>
2025-07-06 07:08:49 +0400

    reusable funtions

---
+++ functions/reusable function.py
@@ -0,0 +1,13 @@
+def is_even(num):
+    if num % 2 == 0:
+        return True
+    else:
+        return False
+    
+num = 1
+num2 = 2
+
+if (is_even(num)):
+    print("even")
+else:
+    print("odd")
