488ea2d672d06be0fef04633f02bd0d70b84b9f6
SlemanKhadoor <s.khadoor@zendy.io>
2025-07-08 09:38:41 +0400

    exception handling using as

--- exception_handling/exception_handling.py
+++ exception_handling/exception_handling.py
@@ -37,3 +37,22 @@ except:
     print("Something went wrong")
 finally:
     print("finished")
+
+print("#-----------------------------------#")
+
+a = 5
+b = 0
+try:
+    z=a/b
+    print(z)
+except ZeroDivisionError as e: 
+    print(f"exception we faced {e} ")
+except Exception as e:  # we used parent class Exception as we don't know what type we are gonna face
+    print(f"exception we faced {e} ")
+else:
+    print("nothing went wrong")
+finally:
+    print("finished")
+
+
+# if we know thar we may face different types of exceptions in the code we can use each one of them in an except block and provide the code needed to be done 
