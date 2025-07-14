ecbd3c5f2a18b2f8c8b141ecfd6559f996dcd5d6
SlemanKhadoor <s.khadoor@zendy.io>
2025-07-14 15:51:35 +0400

    operators

---
+++ data_structures/operators.py
@@ -0,0 +1,22 @@
+#.     Arethmitic Operators
+# We have all the known operations like addition +, subtraction -, multiplication *, float division /
+# we aslo have something called  floor division // which approximate to the upper number
+# We have also the Modulus %  and the Exponent ** (a**2)
+
+#-------------------------------------------------------------
+
+
+#      Cpmparasion Operators
+#
+# <, >, =, !=, <=, >=
+#
+#
+
+
+
+#-------------------------------------------------------------
+
+#         Logical Operators
+#
+#. and, or, not
+#
