fcf33ccfbd143453cedd03a9c0cc45cf4f18f24d
SlemanKhadoor <s.khadoor@zendy.io>
2025-07-20 23:33:51 +0400

    polymorphism

--- OOP/ploymorphism.py
+++ OOP/ploymorphism.py
@@ -6,16 +6,31 @@ class Animal():
     
     def sound(self):
         pass
+
+
 # child class
 class Dog(Animal):
-    pass
+    #override
+    def move(self):
+        print("run")
+    
+    def sound(self):
+        print("bark")
+
+    
 # child class
 class Snake(Animal):
     #override
     def move(self):
         print("crawl")
 
+    def sound(self):
+        print("hiss")
+
 dog = Dog()
 snake = Snake()
 dog.move()
+dog.sound()
 snake.move()
+snake.sound()
+print("#-----------------#")
