d9c9b03e1ac35e10b9703c1cb768dd48caa44baa
SlemanKhadoor <s.khadoor@zendy.io>
2025-07-20 04:07:44 +0400

    tuples

---
+++ data_structures/tuples.py
@@ -0,0 +1,10 @@
+# Tuples are immutable objects
+# They are similar to lists but it can't be changed
+t = (1,3.5,"a")
+print(len(t))
+#------------------------
+print(t[0])
+#------------------------
+print(t[-1])
+#------------------------
+# we can't assign or append as they are immutable
