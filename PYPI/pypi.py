d073e57095924bb63885f42d8ecf7299bc838d44
SlemanKhadoor <s.khadoor@zendy.io>
2025-07-21 00:38:12 +0400

    pypi

---
+++ PYPI/pypi.py
@@ -0,0 +1,7 @@
+# pypi.  stands for python package index
+
+# visit this open source website on.          pypi.org
+
+
+# pip is the package manger for python
+# pip list will list all the packages installed
