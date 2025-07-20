ab564d2c89e3cf7a87829b543c224788117da0db
SlemanKhadoor <s.khadoor@zendy.io>
2025-07-20 05:52:22 +0400

    sets

---
+++ data_structures/sets.py
@@ -0,0 +1,9 @@
+# sets contains unique elements and also sorted elements
+# so if we assign a list to a set it will remove the dublication and sort the elements
+# set functions
+# add()
+# update() used to concatinate two sets
+# remove()
+# union()  same as update but it returns new set 
+# intersection()  det duplicated items from two sets and return them as new set
+# intersection_update()
